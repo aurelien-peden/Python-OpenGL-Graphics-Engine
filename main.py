a1e54fd9865d8e40492c190ba0468a227a082140
aurelien-peden <aurelienpeden@gmail.com>
2024-10-23 14:22:39 +0200

    OpenGL window initialization

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame as pg
+import moderngl as mgl
+import sys
+
+class GraphicsEngine:
+    def __init__(self, win_size=(1600, 900)):
+        # Init pygame modules
+        pg.init()
+        self.WIN_SIZE = win_size
+
+        # Configure OpenGL attributes
+        # Set OpenGL 3.3 core profile
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+        pg.display.set_mode(self.WIN_SIZE, flags=pg.OPENGL | pg.DOUBLEBUF)
+
+        # Detect and use existing OpenGL context
+        self.ctx = mgl.create_context()
+
+        self.clock = pg.time.Clock()
+    
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+
+    def render(self):
+        # clear frame buffer
+        self.ctx.clear(color=(0.08, 0.16, 0.18))
+        # swap buffers
+        pg.display.flip()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.render()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    app = GraphicsEngine()
+    app.run()
